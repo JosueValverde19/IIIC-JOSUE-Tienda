9e633f64ae63b8659864090d3d258d97af7d8004
Josue Valverde <jvalverde40358@ufide.ac.cr>
2023-09-20 20:51:56 -0600

    Semana2

---
+++ Tienda_JosueValverde/src/main/java/Tienda/demo/TiendaJosueValverdeApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJosueValverdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJosueValverdeApplication.class, args);
+	}
+
+}
