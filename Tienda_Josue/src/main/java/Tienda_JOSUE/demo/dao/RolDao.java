921682793046dde6a027ea8cec040d89201638ed
Josue Valverde <jvalverde40358@ufide.ac.cr>
2023-11-22 23:01:02 -0600

    Semana11

---
+++ Tienda_Josue/src/main/java/Tienda_JOSUE/demo/dao/RolDao.java
@@ -0,0 +1,9 @@
+
+package Tienda_JOSUE.demo.dao;
+
+import Tienda_JOSUE.demo.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+    
+}
---
+++ Tienda_Josue/src/main/java/Tienda_JOSUE/demo/service/CorreoService.java
@@ -0,0 +1,14 @@
+
+package Tienda_JOSUE.demo.service;
+
+import jakarta.mail.MessagingException;
+
+public interface CorreoService {
+    
+    public void enviarCorreoHtml(
+            String para, 
+            String asunto, 
+            String contenidoHtml) 
+            throws MessagingException;
+    
+}
